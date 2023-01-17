5130e5952c913cd3d8e1df22fab67c1d70273e7f
Erhan Akça <erhanakca@Erhan-MacBook-Air.local>
2023-01-17 05:34:39 +0300

    date display added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,36 @@
 @extends('layout.app')
 @section('content')
 
+    <div class="container mt-4">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand text-body-secondary">Patch Notes</a>
+                <form class="d-flex" action="#">
+                    <button class="btn btn-success" type="submit">Add New Patch Note</button>
+                </form>
+            </div>
+        </nav>
+
+        <div class="row">
+            <div class='col-sm-6'>
+                <div class="form-group">
+                    <div class='input-group date' id='datetimepicker1'>
+                        <input type='text' class="form-control" id="tarih" />
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+
+            <script>
+                $( function() {
+                    $("#tarih").datepicker();
+                } );
+            </script>
 
 
 
-
+    </div>
 
 
 
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,13 +2,21 @@
 <html lang="tr">
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
     <title>Patch Notes</title>
+    <style>
+
+    </style>
 </head>
-<body>
+<body bgcolor="rgb(182,171,171);">
 @yield('content')
 </body>
 </html>
